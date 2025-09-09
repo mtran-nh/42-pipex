f4ceb599d35ba37a0115ab63abd3af0a725963d1
Mai Chi Tran Nhat <maichibod@gmail.com>
2025-09-09 17:35:19 +0200

    exec and processes

--- include/pipex.h
+++ include/pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtran-nh <mtran-nh@student.42heilbronn.de> +#+  +:+       +#+        */
+/*   By: mtran-nh <mtran-nh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 16:53:43 by mtran-nh          #+#    #+#             */
-/*   Updated: 2025/09/08 19:02:05 by mtran-nh         ###   ########.fr       */
+/*   Updated: 2025/09/09 17:33:12 by mtran-nh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,8 @@ void	free_arr(char **arr);
 void	exit_error(const char *msg, char **need_free);
 char	**split_cmd(char *cmd);
 char	*get_cmd_path(char *cmd, char **envp);
+void	execution(char *cmd, char **envp);
+void	child_process1(int infile, int fd[2], char *cmd, char **envp);
+void	child_process2(int outfile, int fd[2], char *cmd, char **envp);
 
 #endif
--- src/exec.c
+++ src/exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtran-nh <mtran-nh@student.42heilbronn.de> +#+  +:+       +#+        */
+/*   By: mtran-nh <mtran-nh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 13:20:31 by mtran-nh          #+#    #+#             */
-/*   Updated: 2025/09/08 19:06:51 by mtran-nh         ###   ########.fr       */
+/*   Updated: 2025/09/09 17:32:19 by mtran-nh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,28 @@ void	execution(char *cmd, char **envp)
 		free(path);
 		exit_error("execve failed", args);
 	}
+}
+
+void	child_process1(int infile, int fd[2], char *cmd, char **envp)
+{
+	if (dup2(infile, STDIN_FILENO) < 0)
+		exit_error("dup2 infile failed");
+	if (dup2(fd[1], STDOUT_FILENO) < 0)
+		exit_error("dup2 pipe write failed");
+	close (fd[0]);
+	close (fd[1]);
+	close (infile);
+	execution(cmd, envp);
+}
+
+void	child_process2(int outfile, int fd[2], char *cmd, char **envp)
+{
+	if (dup2(fd[0], STDIN_FILENO) < 0)
+		exit_error("dup2 pipe out failed");
+	if (dup2(outfile, STDOUT_FILENO) < 0)
+		exit_error("dup2 outfile failed");
+	close (fd[0]);
+	close (fd[1]);
+	close (outfile);
+	execution (cmd, envp);
 }
